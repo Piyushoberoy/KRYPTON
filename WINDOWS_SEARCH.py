a8ff0942a703b4fbdde654fb97eb3197b03cbe12
Piyush <piyushoberoy14@gmail.com>
2023-10-03 23:47:14 +0530

    VERSION 2

--- WINDOWS_SEARCH.py
+++ WINDOWS_SEARCH.py
@@ -1,19 +1,12 @@
 import os
 import fnmatch
-cnt = 0
-def search_files_in_all_drives(pattern):
+
+def search(pattern):
     found_files = []
 
-    # On Windows, drives are represented as letters (e.g., C:, D:)
     drives = ['%s:' % d for d in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if os.path.exists('%s:' % d)]
     for drive in drives:
         for root, dirs, files in os.walk(drive):
-            # print(root, ",", dirs)
-            # print(files)
-            global cnt
-            print(cnt)
-            cnt += 1
-            
             for filename in fnmatch.filter(files, pattern):
                 found_files.append(os.path.join(root, filename))
 
@@ -22,7 +15,7 @@ def search_files_in_all_drives(pattern):
 if __name__ == "__main__":
     file_pattern = input("Enter the file pattern (e.g., *.txt): ")
 
-    results = search_files_in_all_drives(file_pattern)
+    results = search(file_pattern)
 
     if results:
         print("Found files:")
